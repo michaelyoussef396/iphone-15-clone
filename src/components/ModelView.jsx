362d2bc437dce7476032646b142ccbe38df1e294
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-04-18 01:20:50 +1000

    add modelview component for 3d display

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,22 @@
+import {PerspectiveCamera, View } from "@react-three/drei"
 
+import Lights from './Lights';
 
-const ModelView = () => {
+const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationState, size, item }) => {
   return (
-    <div>
-      
-    </div>
+    <View
+      index={index}
+      id={gsapType}
+      className={`w-full h-full absolute ${index === 2 ? 'right-[-100%]' : ''}`}
+    >
+      {/* Ambient Light */}
+      <ambientLight intensity={0.3} />
+
+      <PerspectiveCamera makeDefault position={[0, 0, 4]} />
+
+      <Lights />
+
+    </View>
   )
 }
 
