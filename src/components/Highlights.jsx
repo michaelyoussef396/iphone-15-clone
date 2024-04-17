d88e86f53da6ef46950cb278e96c2e1286002ca8
michaelyoussef396 <michaelyoussef396@gmail.com>
2024-04-18 00:50:02 +1000

    create videoCarousel component

--- src/components/Highlights.jsx
+++ src/components/Highlights.jsx
@@ -2,6 +2,7 @@ import { useGSAP } from "@gsap/react"
 import gsap from "gsap"
 import { rightImg, watchImg } from "../utils"
 
+import VideoCarousel from './VideoCarousel';
 
 const Highlights = () => {
   useGSAP(() => {
@@ -27,6 +28,7 @@ const Highlights = () => {
           </div>
         </div>
 
+        <VideoCarousel />
       </div>
     </section>
   )
